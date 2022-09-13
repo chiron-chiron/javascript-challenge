285db69ad64353b8f5ce7e3fb915faf38835f833
chiron-chiron <84137247+chiron-chiron@users.noreply.github.com>
2022-09-13 14:33:58 +1000

    done

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,11 +1,41 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-// Get a reference to the table body
-var tbody = d3.select("tbody");
 
-tableData.forEach(ufoSighting) {
-    console.log(ufoSighting):
+// check
+console.log(tableData);
+
+
+// Get a reference to the html elements
+var tbody = d3.select("tbody");
+var button = d3.select("#button");
+var form = d3.select("#form")
+
+
+// Create table for data
+tableData.forEach((ufoSighting) => {
     var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
 });
+
+
+// Create event handlers
+button.on("click", runFilter);
+form.on("submit", runFilter);
+
+function runFilter(){
+    var filterInput = d3.select("#datetime").property("value");
+    tbody.html("");
+    d3.event.preventDefault();
+    var filterData = tableDate.filter(x => x.datetime == filterInput);
+    filteredData.forEach((ufoSighting) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoSighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
